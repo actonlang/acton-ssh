891e802b721fd8752f29805766abff87f6f092a8
Antonio Prcela <antonio.prcela@sartura.hr>
2024-11-08 08:38:19 +0100

    create initial Channel actor

--- src/ssh.ext.c
+++ src/ssh.ext.c
@@ -2,6 +2,8 @@
 #include <libssh/libssh_version.h>
 // TODO: figure out how to include rts/log so we get access to log_error etc
 
+#define LOG_ERR(msg) printf("ERR\t%s\n", (msg))
+
 void noop_free(void *ptr) {
 }
 
@@ -19,13 +21,16 @@ void sshQ___ext_init__() {
         acton_gc_strdup,
         acton_gc_strndup);
     int r = ssh_init();
-    printf("SSH extension initialized %d\n", r);
+    if (r != SSH_OK)
+      printf("SSH init failed (%d)\n", r);
+    else
+      printf("SSH extension successfully initialized (retval: %d)\n", r);
 }
 
 B_str sshQ_version () {
-    if (LIBSSH_VERSION_MINOR != 11)
-        return to$str("invalid");
-    return to$str("0.11.0");
+    if (LIBSSH_VERSION_MAJOR != 0 || LIBSSH_VERSION_MINOR != 11 || LIBSSH_VERSION_MICRO != 0)
+        return to$str("unsupported version");
+    return to$str("libssh 0.11.0 supported\n");
 }
 
 // TODO: crap function for test, to be replaced with something
@@ -47,7 +52,7 @@ int show_remote_processes(ssh_session session)
     return rc;
   }
 
-  rc = ssh_channel_request_exec(channel, "ps aux");
+  rc = ssh_channel_request_exec(channel, "uptime");
   if (rc != SSH_OK)
   {
     ssh_channel_close(channel);
@@ -81,22 +86,56 @@ int show_remote_processes(ssh_session session)
   return SSH_OK;
 }
 
+$R sshQ_ChannelD__initG_local (sshQ_Channel self, $Cont c$cont) {
+    int err = 0;
+    ssh_channel channel = ssh_channel_new(self->_ssh_session);
+    if (channel == NULL) {
+        LOG_ERR("Failed to create SSH channel");
+        printf("ssh_get_error: %s\n\n" , ssh_get_error(self->_ssh_session));
+        return $R_CONT(c$cont, B_None);
+    }
+
+    printf("\t%s channel: %p\n", __FUNCTION__, channel);
+    self->_ssh_channel = channel;
+    printf("\t%s self->_ssh_channel:\t%p\n", __FUNCTION__, self->_ssh_channel);
+
+    err = ssh_channel_open_session(channel);
+    if (err != SSH_OK) {
+        printf("\t%s ssh_channel_open_session() error (%d)\n", __FUNCTION__, err);
+        return $R_CONT(c$cont, B_None);
+    }
+    if (ssh_channel_request_exec(channel, "touch /tmp/bla")) {
+        printf("\t%s Error executing '%s' : %s\n", __FUNCTION__, "touch /tmp/bla", ssh_get_error(self->_ssh_session));
+        // ssh_channel_free(channel);
+        return $R_CONT(c$cont, B_None);
+    }
+    ssh_channel_send_eof(channel);
+    ssh_channel_close(channel);
+    ssh_channel_free(channel);
+
+    return $R_CONT(c$cont, B_None);
+}
+
 $R sshQ_ClientD__initG_local (sshQ_Client self, $Cont c$cont) {
     ssh_session session = ssh_new();
     if (session == NULL) {
-        //log_error("Failed to create SSH session");
+        LOG_ERR("Failed to create SSH session");
         return $R_CONT(c$cont, B_None);
     }
-    printf("session: %p\n", session);
-    self->_ssh_session = toB_u64((unsigned long)session);
-    printf("init self->session: %p\n", self->_ssh_session);
+    // casting via toB_u64((unsigned long)..) leads to an invalid value in self->_ssh_session
+    // self->_ssh_session = toB_u64((unsigned long)session);
+    // instead do direct assignment
+    self->_ssh_session = session;
+
+    printf("\t%s session:\t\t%p\n", __FUNCTION__, session);
+    printf("\t%s self->session:\t%p\n", __FUNCTION__, self->_ssh_session);
 
     ssh_options_set(session, SSH_OPTIONS_HOST, fromB_str(self->host));
     ssh_options_set(session, SSH_OPTIONS_PORT, &self->port->val);
     ssh_options_set(session, SSH_OPTIONS_USER, fromB_str(self->username));
 
     ssh_set_blocking(session, 1);
-    printf("Connecting to \n");
+    printf("Connecting to SSH server '%s'\n", fromB_str(self->host));
     int rc = ssh_connect(session);
     if (rc != SSH_OK) {
         //log_error("Error connecting to SSH server: %s", ssh_get_error(session));
@@ -107,7 +146,7 @@ $R sshQ_ClientD__initG_local (sshQ_Client self, $Cont c$cont) {
 
     rc = ssh_userauth_password(session, NULL, fromB_str(self->password));
     if (rc == SSH_OK) {
-        printf("Connected\n");
+        ($action) self->on_connect;
         show_remote_processes(session);
     } else {
         printf("Error: %s\n", ssh_get_error(session));
