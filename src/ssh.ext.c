27b22581c9426341ec62ab801c8e1c094ca271ce
Antonio Prcela <antonio.prcela@sartura.hr>
2025-02-28 13:00:56 +0100

    add affiinity and return the retreived string to caller

--- src/ssh.ext.c
+++ src/ssh.ext.c
@@ -3,11 +3,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <rts/rts.h>
 
 #ifndef DEBUG_MODE
-//#define DEBUG_MODE    // uncomment for pretty prints
+#define DEBUG_MODE    /* uncomment for pretty prints */
 #endif
 
+#define BUF_SIZE 65536
 #define TIMEOUT 1500000         // microseconds: 1.5 seconds
 #define USLEEP_INTERVAL 5000    // microseconds: 0.005 seconds
 
@@ -58,17 +60,29 @@ B_str sshQ_version() {
     return to$str("0.1.0");
 }
 
+$R sshQ_ClientD__pin_affinityG_local (sshQ_Client self, $Cont c$cont) {
+    pin_actor_affinity();
+    return $R_CONT(c$cont, B_None);
+}
+
+$R sshQ_ChannelD__pin_affinityG_local (sshQ_Channel self, $Cont c$cont) {
+    pin_actor_affinity();
+    return $R_CONT(c$cont, B_None);
+}
+
 /**
  * @brief Send netconf payload
  *
  * @param[in] channel ssh_channel
  * @param[in] payload The Netconf Payload, for example the hello message
  */
-int send_nc_payload(ssh_channel channel, const char *payload)
+int send_nc_payload(ssh_channel channel, const char *payload, char *buf)
 {
-    char buffer[4096] = { 0 };
     int rc = 0;
     int nbytes = 0;
+    size_t buflen = 0;
+    size_t len = 0;
+    char tmp[1024] = {0};
 
     rc = ssh_channel_write(channel, payload, strlen(payload) + 1);
     if (rc == SSH_ERROR)
@@ -78,20 +92,49 @@ int send_nc_payload(ssh_channel channel, const char *payload)
         return rc;
     }
 
-    nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+    nbytes = ssh_channel_read(channel, tmp, sizeof(tmp), 0);
     while (nbytes > 0)
     {
-        if (write(STDOUT_FILENO, buffer, nbytes) != (unsigned int) nbytes)
+#ifdef DEBUG_MODE
+        printf("\n\nstrlen buf: %u\n", strlen(buf));
+        printf("strlen tmp: %u\n", strlen(tmp));
+        printf("sizeof tmp: %u\n", sizeof(tmp));
+#endif
+        // sometimes the string tmp has invalid characters from the 1024th element
+        if (strlen(tmp) > sizeof(tmp)) {
+            tmp[1024] = '\0';
+        }
+
+        // append string
+        len = snprintf(buf + buflen, BUF_SIZE - buflen, "%s", tmp);
+        buflen = strlen(buf);
+
+#ifdef DEBUG_MODE
+        // TODO: sometimes the strlen() of buf is lower than in the previous iteration
+        // which breaks the appending and the result is invalid
+        printf("strlen buffer after snprintf: %u\n\n", buflen);
+#endif
+        if (len > BUF_SIZE)
+        {
+            printf("%s: snprintf() error %u\n", __FUNCTION__, buflen);
+            ssh_close_free(channel);
+            return SSH_ERROR;
+        }
+#ifdef DEBUG_MODE
+        if (write(STDOUT_FILENO, tmp, nbytes) != (unsigned int) nbytes)
         {
             printf("%s: write() error (bytes written not matching expectation)\n", __FUNCTION__);
             ssh_close_free(channel);
             return SSH_ERROR;
         }
+        fflush(stdout);
+#endif
         // find end of netconf reply
-        if (strstr(buffer, "]]>]]>")) {
-            break;
+        if (strstr(tmp, "]]>]]>")) {
+            return SSH_OK;
         }
-        nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+        memset(tmp, 0, sizeof(tmp));
+        nbytes = ssh_channel_read(channel, tmp, sizeof(tmp), 0);
     }
 
     if (nbytes < 0)
@@ -101,11 +144,14 @@ int send_nc_payload(ssh_channel channel, const char *payload)
         return SSH_ERROR;
     }
 
-    ssh_close_free_eof(channel);
     return SSH_OK;
 }
 
+// Client
+
 $R sshQ_ClientD__initG_local (sshQ_Client self, $Cont c$cont) {
+    pin_actor_affinity();
+
     int err = 0;
     ssh_session session = ssh_new();
     if (session == NULL)
@@ -143,7 +189,16 @@ $R sshQ_ClientD__initG_local (sshQ_Client self, $Cont c$cont) {
     return $R_CONT(c$cont, B_None);
 }
 
+$R sshQ_ClientD_disconnectG_local (sshQ_Client self, $Cont c$cont) {
+    ssh_disconnect((ssh_session)fromB_u64(self->_ssh_session));
+    return $R_CONT(c$cont, B_None);
+}
+
+// Channel
+
 $R sshQ_ChannelD__initG_local (sshQ_Channel self, $Cont c$cont) {
+    pin_actor_affinity();
+
     int err = 0;
     ssh_session session = { 0 };
     ssh_channel channel = { 0 };
@@ -192,6 +247,7 @@ $R sshQ_ChannelD__initG_local (sshQ_Channel self, $Cont c$cont) {
 $R sshQ_ChannelD_sendPayloadG_local (sshQ_Channel self, $Cont c$cont) {
     int err = 0;
     int timeout = TIMEOUT;
+    char *buffer = (char*)acton_calloc(0, BUF_SIZE * sizeof(char));
     ssh_channel channel = (ssh_channel)fromB_u64(self->_ssh_channel);
 
     if (self->_subsystem && !strcmp((const char *)fromB_str(self->_subsystem), "netconf")) {
@@ -200,24 +256,27 @@ $R sshQ_ChannelD_sendPayloadG_local (sshQ_Channel self, $Cont c$cont) {
             err = usleep(USLEEP_INTERVAL);
             if (err) {
                 printf("%s: usleep() error '%s' (%d)\n", __FUNCTION__, strerror(errno), errno);
-                return $R_CONT(c$cont, B_None);
+                // return $R_CONT(c$cont, B_None);
             }
             timeout -= USLEEP_INTERVAL;
         }
         if (err != SSH_OK)
         {
             printf("%s: ssh_channel_request_subsystem() Error setting SSH subsystem 'netconf': %d\n", __FUNCTION__, err);
-            return $R_CONT(c$cont, B_None);
+            // return $R_CONT(c$cont, B_None);
         }
 
-        err = send_nc_payload(channel, (const char *)fromB_str(self->payload));
+        err = send_nc_payload(channel, (const char *)fromB_str(self->payload), buffer);
         if (err != SSH_OK)
         {
             printf("%s: send_nc_payload() error: %d\n", __FUNCTION__, err);
-            return $R_CONT(c$cont, B_None);
+            // return $R_CONT(c$cont, B_None);
         }
     }
 
     ssh_close_free_eof(channel);
-    return $R_CONT(c$cont, B_None);
+#ifdef DEBUG_MODE
+    printf("\n\nbuffer:\n%s\n\n", buffer);
+#endif
+    return $R_CONT(c$cont, to$str(buffer));
 }
